16868df0ae22a9a4da81c2bbb080b7739acca621
Fernando Perez <fernandohps17@gmail.com>
2023-12-09 19:22:59 -0400

    Se crearon las funcion de agregar, actualizar, eliminar y completar todos

--- src/Hooks/useTodo.js
+++ src/Hooks/useTodo.js
@@ -1,1 +1,65 @@
-// minuto 24 segundo 24
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../todoReducer"
+
+export const useTodo = () => {
+
+    const initialState = []
+
+    // Funcion para obtener el localStorage
+    const init = () => {
+        return JSON.parse()
+        // minuto 33 segundo 18
+    }
+
+    const [state, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+    // Funcion para agregar un Todo
+    const handleNewTodo = todo => {
+        const action = {
+            type: "Add Todo",
+            payload: todo,
+        }
+        dispatch(action)
+    }
+
+    // Funcion para eliminar un Todo
+    const handleDeleteTodo = id => {
+        const action = {
+            type: "Delete Todo",
+            payload: id
+        }
+        dispatch(action)
+    }
+
+    // Funcion para completar un Todo
+    const handleCompleteTodo = id => {
+        const action = {
+            type: "CompleteTodo",
+            payload: id
+        }
+        dispatch(action)
+    }
+
+    // Funcion para actualizar un Todo
+    const handleUpdateTodo = (id, description) => {
+        const action = {
+            type: "UpdateTodo",
+            payload: {
+                id,
+                description
+            }
+        }
+        dispatch(action)
+    }
+
+    return {
+        handleNewTodo,
+        handleDeleteTodo,
+        handleCompleteTodo,
+        handleUpdateTodo
+    }
+}
